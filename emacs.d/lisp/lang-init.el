3f4b82f9de7ef6101fb3154f31d404226800e169
Jame298 <jame2981@gmail.com>
2014-10-31 10:27:56 +0800

    add ruby go haml modes

---
+++ emacs.d/lisp/lang-init.el
@@ -0,0 +1,27 @@
+;; Flymake
+(add-to-list 'load-path "~/.emacs.d/lang/flymake")
+(require 'flymake-easy)
+
+;; Ruby
+(add-to-list 'load-path "~/.emacs.d/lang/ruby")
+(require 'flymake-ruby)
+(autoload 'enh-ruby-mode "enh-ruby-mode" "Major mode for ruby files" t)
+(add-to-list 'auto-mode-alist '("\\.(rb|rake|ru|god)$" . enh-ruby-mode))
+(add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
+(add-hook 'enh-ruby-mode-hook 'flymake-ruby-load)
+
+;; Golang
+(add-to-list 'load-path "~/.emacs.d/lang/golang")
+(require 'go-mode-autoloads)
+(require 'flymake-go)
+
+;; Haml
+(add-to-list 'load-path "~/.emacs.d/lang/haml")
+(require 'haml-mode)
+(require 'flymake-haml)
+(add-hook 'haml-mode-hook 'flymake-haml-load)
+
+
+(provide 'lang-init)
+
+
