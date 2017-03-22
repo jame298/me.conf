11a90606cadd97946148d8e15d3818b57420d91a
Jame298 <jame2981@gmail.com>
2017-03-22 10:42:15 +0800

    change yas-next-field.

--- emacs.d/lisp/yasnippet-init.el
+++ emacs.d/lisp/yasnippet-init.el
@@ -7,8 +7,11 @@
 ;; Remove Yasnippet's default tab key binding
 (define-key yas-minor-mode-map (kbd "<tab>") nil)
 (define-key yas-minor-mode-map (kbd "TAB") nil)
+(define-key yas-keymap (kbd "<tab>") nil)
+(define-key yas-keymap (kbd "TAB") nil)
 
 ;; Set Yasnippet's key binding to shift+tab
 (define-key yas-minor-mode-map (kbd "<backtab>") 'yas-expand)
+(define-key yas-keymap (kbd "<backtab>") 'yas-next-field)
 
 (provide 'yasnippet-init)
